37a1e53755b15b40477530ad7dbebadf09c7e5f2
chintanvora-ios <chintan.pitron@gmail.com>
2025-05-08 11:20:20 +0530

    --
    --

--- Sources/BaseKit/Extension/UILabel.swift
+++ Sources/BaseKit/Extension/UILabel.swift
@@ -2,14 +2,17 @@ import UIKit
 
 
 public extension UILabel {
-    func setHTMLFromString(_ htmlText: String) {
+    func setHTMLFromString(_ htmlText: String, textColor: UIColor? = nil) {
         guard let data = htmlText.data(using: .utf8) else { return }
         let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
             .documentType: NSAttributedString.DocumentType.html,
             .characterEncoding: String.Encoding.utf8.rawValue
         ]
 
-        if let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) {
+        if let attributedString = try? NSMutableAttributedString(data: data, options: options, documentAttributes: nil) {
+            if let color = textColor {
+                attributedString.addAttribute(.foregroundColor, value: color, range: NSRange(location: 0, length: attributedString.length))
+            }
             self.attributedText = attributedString
         }
     }
