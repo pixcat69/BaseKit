81b371780a2743728b8edeb45815dafd64969015
chintanvora-ios <chintan.pitron@gmail.com>
2025-05-09 18:21:31 +0530

    update code
    add new func in UILabel

--- Sources/BaseKit/Extension/UILabel.swift
+++ Sources/BaseKit/Extension/UILabel.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SafariServices
 
 
 public extension UILabel {
@@ -15,5 +16,59 @@ public extension UILabel {
             }
             self.attributedText = attributedString
         }
+    }
+    
+    func enableURLLinks() {
+        guard let text = self.text else { return }
+
+        let types: NSTextCheckingResult.CheckingType = .link
+        let detector = try? NSDataDetector(types: types.rawValue)
+        let matches = detector?.matches(in: text, options: [], range: NSRange(location: 0, length: text.utf16.count)) ?? []
+
+        let attributedText = NSMutableAttributedString(string: text)
+        for match in matches {
+            guard let url = match.url else { continue }
+            attributedText.addAttribute(.link, value: url, range: match.range)
+        }
+
+        self.attributedText = attributedText
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(openURLFromLabel)))
+    }
+
+    @objc private func openURLFromLabel(_ recognizer: UITapGestureRecognizer) {
+        guard let text = self.attributedText?.string else { return }
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: self.bounds.size)
+        let textStorage = NSTextStorage(attributedString: self.attributedText ?? NSAttributedString())
+
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+
+        let location = recognizer.location(in: self)
+        textContainer.lineFragmentPadding = 0
+        textContainer.maximumNumberOfLines = self.numberOfLines
+        textContainer.lineBreakMode = self.lineBreakMode
+
+        let index = layoutManager.characterIndex(for: location, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+
+        if index < textStorage.length {
+            let attributes = textStorage.attributes(at: index, effectiveRange: nil)
+            if let url = attributes[.link] as? URL, let vc = self.findViewController() {
+                let safariVC = SFSafariViewController(url: url)
+                vc.present(safariVC, animated: true)
+            }
+        }
+    }
+
+    private func findViewController() -> UIViewController? {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let vc = responder as? UIViewController {
+                return vc
+            }
+            responder = responder?.next
+        }
+        return nil
     }
 }
