da9e0aa7d8e9d8d790c7dc9767ead3f3ca50c934
chintanvora-ios <chintan.pitron@gmail.com>
2025-05-10 10:12:43 +0530

    New Extension
    Adding new UIColor extension for hexa color

--- Sources/BaseKit/Extension/Comman.swift
+++ Sources/BaseKit/Extension/Comman.swift
@@ -79,3 +79,28 @@ public extension Double {
         return Double(Int(self * divisor)) / divisor
     }
 }
+
+
+//MARK: UIColor
+public extension UIColor {
+    convenience init?(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        // Remove leading "#" if present
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted.removeFirst()
+        }
+
+        // Must be 6 characters now
+        guard hexFormatted.count == 6,
+              let rgbValue = UInt32(hexFormatted, radix: 16) else {
+            return nil
+        }
+
+        let red = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgbValue & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
