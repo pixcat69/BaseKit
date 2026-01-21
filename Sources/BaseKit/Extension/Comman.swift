03ef1a6a388abc3d97149d10c3caeb8846d28b1c
chintanvora-ios <chintan.pitron@gmail.com>
2026-01-21 17:34:32 +0530

    Add extension
    Adding new UIApplication extension

--- Sources/BaseKit/Extension/Comman.swift
+++ Sources/BaseKit/Extension/Comman.swift
@@ -120,3 +120,20 @@ public extension String {
         return url.scheme == "http" || url.scheme == "https"
     }
 }
+
+
+//MARK: - UIApplication
+public extension UIApplication {
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController, let selected = tab.selectedViewController {
+            return topViewController(base: selected)
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+        return base
+    }
+}
