d93c9d04e859fac1f69937c9db7554e0522964e8
chintanvora-ios <chintan.pitron@gmail.com>
2026-06-02 12:06:03 +0530

    Update UIView
    Add apply shadow feature

--- Sources/BaseKit/Extension/UIView.swift
+++ Sources/BaseKit/Extension/UIView.swift
@@ -14,6 +14,73 @@ public extension UIView {
         }
     }
     
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            guard let color = layer.shadowColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+            layer.masksToBounds = false
+        }
+    }
+
+    @IBInspectable var shadowOffsetWidth: CGFloat {
+        get {
+            return layer.shadowOffset.width
+        }
+        set {
+            layer.shadowOffset = CGSize(width: newValue, height: layer.shadowOffset.height)
+        }
+    }
+
+    @IBInspectable var shadowOffsetHeight: CGFloat {
+        get {
+            return layer.shadowOffset.height
+        }
+        set {
+            layer.shadowOffset = CGSize(width: layer.shadowOffset.width, height: newValue)
+        }
+    }
+
     func specificSide(corners: UIRectCorner, radius: CGFloat) {
         DispatchQueue.main.async { [self] in
             let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
@@ -35,26 +102,6 @@ public extension UIView {
         self.layer.cornerRadius = corner
         self.layer.borderColor = borderColor.cgColor
         self.layer.borderWidth = borderWidth
-    }
-    
-    
-    @IBInspectable var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-    
-    @IBInspectable var borderColor: UIColor? {
-        get {
-            guard let color = layer.borderColor else { return nil }
-            return UIColor(cgColor: color)
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
-        }
     }
     
     
